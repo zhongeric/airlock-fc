943f8151aa670adfbffb2ee691fa12e7359c816f
Eric Zhong <eric.zhong@uniswap.org>
2024-01-30 19:41:01 -0500

    Allow for custom params

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,42 @@
 import { getFrameMetadata } from '@coinbase/onchainkit';
-import type { Metadata } from 'next';
-import { BASE_URL } from './api/frame/route';
+import type { Metadata, ResolvingMetadata } from 'next';
+import { BASE_URL } from '../app/api/frame/route';
 
 const TITLE = "Airlock"
 
-const frameMetadata = getFrameMetadata({
-  buttons: ['Check eligibility'],
-  image: 'https://zizzamia.xyz/park-1.png',
-  post_url: BASE_URL,
-});
+type Props = {
+  params: { slug: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
 
-export const metadata: Metadata = {
-  title: TITLE,
-  description: 'LFG',
-  openGraph: {
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const slug = searchParams.slug
+ 
+  // fetch data
+  // const product = await fetch(`https://.../${id}`).then((res) => res.json())
+
+  const frameMetadata = getFrameMetadata({
+    buttons: ['Check eligibility'],
+    image: 'https://zizzamia.xyz/park-1.png',
+    post_url: `${BASE_URL}?slug=${slug}`,
+  });
+ 
+  return {
     title: TITLE,
     description: 'LFG',
-    images: ['https://zizzamia.xyz/park-1.png'],
-  },
-  other: {
-    ...frameMetadata,
-  },
-};
-
-export default function Page() {
-  return (
-    <>
-      <h1>Airlock</h1>
-    </>
-  );
+    openGraph: {
+      title: TITLE,
+      description: 'LFG',
+      images: ['https://zizzamia.xyz/park-1.png'],
+    },
+    other: {
+      ...frameMetadata,
+    },
+  };
 }
+
+export default function Page({ params, searchParams }: Props) {}
