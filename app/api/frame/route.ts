1c55f6c3d9b739f515a381f603196e216c7b251c
Eric Zhong <eric.zhong@uniswap.org>
2024-01-30 19:03:12 -0500

    Wire together frame

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,6 +1,6 @@
 import { FrameRequest, getFrameAccountAddress } from '@coinbase/onchainkit';
-import { kv } from '@vercel/kv';
 import { NextRequest, NextResponse } from 'next/server';
+import { db } from '../kysely';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   let accountAddress: string | undefined = '';
@@ -15,34 +15,50 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="https://zizzamia.xyz/park-2.png" />
     <meta property="fc:frame:button:1" content="${accountAddress}" />
-    <meta property="fc:frame:post_url" content="https://zizzamia.xyz/api/frame" />
+    <meta property="fc:frame:post_url" content="" />
   </head></html>`);
 }
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
-  const body: {
-    price: string;
-    token: string;
-    chain_id: string;
-    proposer: string;
-  } = await req.json();
-  const proposedPrice = body.price;
-  const token = body.token;
-
-  await kv.set(`tokens:${token}_${body.chain_id}`, proposedPrice);
-
-
+  return getResponse(req);
 }
 
-export async function GET(request: NextRequest) {
-  const prices = await kv.hgetall('tokens:USDC');
-  
-  return NextResponse.json(
-    prices,
-    {
-      status: 200,
-    },
-  );
+// This is the frame URL
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const slug = searchParams.get('slug');
+
+  if(!slug) {
+    return new NextResponse(`<!DOCTYPE html><html><head>
+      <meta property="fc:frame" content="vNext" />
+      <meta property="fc:frame:image" content="https://zizzamia.xyz/park-2.png" />
+      <meta property="fc:frame:button:1" content="Project not found" />
+      <meta property="fc:frame:post_url" content="" />
+    </head></html>`);
+  }
+
+  const result = await db
+    .selectFrom('projects')
+    .selectAll()
+    .where('slug', '=', slug)
+    .executeTakeFirst();
+
+  if(!result) {
+    return new NextResponse(`<!DOCTYPE html><html><head>
+      <meta property="fc:frame" content="vNext" />
+      <meta property="fc:frame:image" content="https://zizzamia.xyz/park-2.png" />
+      <meta property="fc:frame:button:1" content="Project not found" />
+      <meta property="fc:frame:post_url" content="" />
+    </head></html>`);
+  }
+
+  // return with project image and name
+  return new NextResponse(`<!DOCTYPE html><html><head>
+      <meta property="fc:frame" content="vNext" />
+      <meta property="fc:frame:image" content="${result.image}" />
+      <meta property="fc:frame:button:1" content="${result.name}" />
+      <meta property="fc:frame:post_url" content="" />
+    </head></html>`);
 }
 
 export const dynamic = 'force-dynamic';
